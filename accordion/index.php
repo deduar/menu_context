e8ea784c2d2029620832f94fb5d9dbbbb3107d33
deduar <deduar@gmail.com>
2021-10-11 12:32:51 -0400

    accordion

--- accordion/index.php
+++ accordion/index.php
@@ -14,7 +14,7 @@
   <div class="panel">
     <button class="accordion">Section 1.1</button>
     <div class="panel">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p>Lorem ipsum dolor sit amet</p>
     </div>
     <button class="accordion">Section 1.2</button>
     <div class="panel">
@@ -27,12 +27,12 @@
 
   <button class="accordion">Section 2</button>
   <div class="panel">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <p>Lorem ipsum dolor sit amet</p>
   </div>
 
   <button class="accordion">Section 3</button>
   <div class="panel">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <p>Lorem ipsum dolor sit amet</p>
   </div>
 
   <script src="app.js"></script>
